23fa0935df825d696c6990590d615a13f02dc624
Tokarev Ilia <upside2002@mail.ru>
2018-04-23 22:57:33 +1100

    added parser yaml

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,9 +2,17 @@ import fs from 'fs';
 import genDiff from '../src';
 
 test('compare json', () => {
-  const pathToAfter = '__tests__/__fixtures__/before.json';
-  const pathToBefore = '__tests__/__fixtures__/after.json';
+  const pathToBefore = '__tests__/__fixtures__/before.json';
+  const pathToAfter = '__tests__/__fixtures__/after.json';
   const pathToDifference = '__tests__/__fixtures__/difference';
   const difference = fs.readFileSync(pathToDifference, 'utf8');
-  expect(genDiff(pathToAfter, pathToBefore)).toBe(difference);
+  expect(genDiff(pathToBefore, pathToAfter)).toBe(difference);
+});
+
+test('compare yaml', () => {
+  const pathToBefore = '__tests__/__fixtures__/before.yaml';
+  const pathToAfter = '__tests__/__fixtures__/after.yaml';
+  const pathToDifference = '__tests__/__fixtures__/difference';
+  const difference = fs.readFileSync(pathToDifference, 'utf8');
+  expect(genDiff(pathToBefore, pathToAfter)).toBe(difference);
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import fs from 'fs';
 import _ from 'lodash';
+import yaml from 'js-yaml';
+import path from 'path';
 
 export default (path1, path2) => {
+  const selectParser = {
+    json: JSON.parse,
+    yaml: yaml.safeLoad,
+  };
+  const parser = selectParser[path.extname(path1).slice(1)];
   const file1 = fs.readFileSync(path1, 'utf8');
   const file2 = fs.readFileSync(path2, 'utf8');
-  const obj1 = JSON.parse(file1);
-  const obj2 = JSON.parse(file2);
+  const obj1 = parser(file1);
+  const obj2 = parser(file2);
   const keys = [...Object.keys(obj1), ...Object.keys(obj2)]
     .reduce((acc, key) => (acc.includes(key) ? acc : [...acc, key]), []);
   const result = keys.reduce((acc, key) => {
