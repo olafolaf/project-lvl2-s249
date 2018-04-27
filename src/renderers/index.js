c446854e73223e0722d762240c6e1fe76e7c5ac8
Tokarev Ilia <upside2002@mail.ru>
2018-04-27 17:42:29 +1100

    added json

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,9 +1,11 @@
 import render from './render';
 import renderPlain from './renderPlain';
+import renderJson from './renderJson';
 
 const renderers = {
   general: render,
   plain: renderPlain,
+  json: renderJson,
 };
 
 export default (data, format) => renderers[format](data);
